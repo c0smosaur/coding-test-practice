29451594179125354fcb1129237bcc3079eb4185
Seo Jean Kang <119040660+c0smosaur@users.noreply.github.com>
2024-09-23 00:11:04 -0700

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+d = deque()
+n = int(input())
+
+for i in range(n):
+    cmd = input().split()
+    if cmd[0] == "push":
+        d.append(cmd[-1])
+    elif cmd[0] == "pop":
+        print(d.popleft() if d else -1)
+    elif cmd[0] == "size":
+        print(len(d))
+    elif cmd[0] == "empty":
+        print(1 if not d else 0)
+    elif cmd[0] == "front":
+        print(d[0] if d else -1)
+    elif cmd[0] == "back":
+        print(d[-1] if d else -1)
