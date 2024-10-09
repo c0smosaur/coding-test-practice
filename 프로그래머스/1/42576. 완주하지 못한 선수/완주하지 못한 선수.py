66ab8cf5fbee948144cacfad4c8538f16659bb06
Seo Jean Kang <119040660+c0smosaur@users.noreply.github.com>
2024-10-09 07:33:55 -0700

    [level 1] Title: 완주하지 못한 선수, Time: 44.68 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,12 @@
+def solution(participant, completion):
+    dic = {}
+    
+    for p in participant:
+        dic[p] = dic.get(p, 0) + 1
+    
+    for c in completion:
+        dic[c] -= 1
+    
+    for k, v in dic.items():
+        if v > 0:
+            return k        
